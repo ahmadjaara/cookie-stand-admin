049156daa428775c99ea9eb1df6e69577562ff12
ahmadjaara <ahmadjaara98@yahoo.com>
2022-06-19 09:58:01 +0300

    react3

--- cookie-stand-admin/pages/components/table.js
+++ cookie-stand-admin/pages/components/table.js
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+
 export default function TableReport (props){
     if (props.dataj.length==[]) return (<h3 className="flex justify-center text-2xl tex-white" >NO COOKIESTAND AVAILABLE</h3>)
     return(
@@ -6,16 +8,24 @@ export default function TableReport (props){
             <tr>
               <th className="border border-green-600 bg-emerald-500">Location</th>
               
-              {props.hours.map(item=>(<th key={item.toString() } className="border border-green-600 bg-emerald-500"> {item} </th>))}
+              {props.hours.map(item=>(<th key={item.id } className="border border-green-600 bg-emerald-500"> {item} </th>))}
               <th className="border border-green-600 bg-emerald-500">total</th> 
             </tr>
           </thead>
           <tbody>
-          {props.dataj.map(listno=>(
-              <tr key={listno.id}>
-                  <td className="border border-green-600 bg-emerald-300"  >{listno.location}</td>
+          {props.dataj.map((listno,index)=>(
+              <tr key={index}>
+                  <td className="border border-green-600 bg-emerald-300"  >
+                    {listno.location} 
+                  <button onClick={e => props.handleDelete(index,e)}>
+                    {console.log(index)}
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                  </svg>
+                  </button> 
+                  </td>
                   {/* {(props.dataj["time"]).map(item=>(<td key={item.toString()} className="border border-green-600"> {item} </td>))} */}
-                  {listno.time?.map(ele=>(<td className="border border-green-600 bg-emerald-300" key={ele.index}> {ele} </td>))}
+                  {listno.time?.map(ele=>(<td className="border border-green-600 bg-emerald-300" key={index}> {ele} </td>))}
                   <td className="border border-green-600 bg-emerald-300" >{listno.total}</td>
 
               </tr>
--- cookie-stand-admin/pages/index.js
+++ cookie-stand-admin/pages/index.js
@@ -5,11 +5,17 @@ import Footer from "./components/Footer"
 import { useState } from "react"
 import { hours } from "../data"
 import TableReport from "./components/table"
+import LoginForm from "./components/loginForm"
 function Home(){
+  const[id,setid]=useState(0)
   const [location,setLocation] =useState("")
   const [Minimum,setMinimum] =useState("")
   const [Maximum,setMaximum] =useState("")
   const [Average,setAverage] =useState("")
+  const [username,setUsername] =useState("")
+  const [password,setPassword] =useState("")
+  const[approved,setapproved]=useState(true)
+ 
   // const [dataj,setdata]=useState("")
   // const [wait,setwait]=useState("")
 const handlesetLocation = (location) => {
@@ -24,6 +30,12 @@ const handlsetMaximum = (Maximum) => {
 const handlsetAverage = (Average) => {
   setAverage(Average)
 }
+const handlsetusername = (username) => {
+  setUsername(username)
+}
+const handlsetpassword = (password) => {
+  setPassword(password)
+}
 function randomInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
@@ -32,19 +44,47 @@ const [dataj,setdata]=useState([])
 const [totalsum,settotalsum]=useState([...Array(14)].map(x =>0))
 const [total,settotal]=useState(0)
 const [numberofitem,setNoOfItem]=useState(0)
+const handelSubmitlogout = (e) =>{
+  e.preventDefault()
+  setapproved(true)
+  setUsername("")
+  setPassword("")
+}
 
+// const[deleteEle,setdelete]=useState(dataj)
+const handleDelete = (index,e) => {
+  e.preventDefault()
+  setdata(dataj.filter((v, i) => i !== index));
+  settotal(total-dataj[index].total)
+  settotalsum([...totalsum].map((e,i)=> e - dataj[index].time[i]))
+  setNoOfItem(dataj.length-1)
+}
+
+  const handelloginSubmit = (e) =>{
+    e.preventDefault()
+    const secretdata=[username,password]
+    if (username=="admin" && password=="1234"){
+      setapproved(false)
+    } 
+
+
+  }
   const handelSubmit = (e) =>{
     e.preventDefault()
     const data=[location,parseInt(Minimum),parseInt(Maximum),Average]
     const listcreator= [...Array(14)].map(x => randomInteger(data[1],data[2]));
     const string={
+      id:id,
       location:location,
       time:listcreator,
       total:listcreator.reduce((a, b) => a + b, 0)
     }
+    setid(string.id+1)
     settotal(total+string.total)
     settotalsum([...totalsum].map((e,i)=> e + string["time"][i]))
     setNoOfItem(dataj.length+1)
+    console.log(dataj)
+    
 
 
     // const string="{"+"location"+":"+" "+location+","+"minCustomers"+":"+" "+Minimum+","+"maxCustomers"+":"+" "+Maximum+","+"avgCookies"+":"+" "+Average+"}"
@@ -64,11 +104,18 @@ const [numberofitem,setNoOfItem]=useState(0)
     setdata([...dataj, string])
 
   }
-    
+   if (approved) return (
+   <LoginForm 
+    username={username} 
+    password={password} 
+    handlsetusername={handlsetusername} 
+    handlsetpassword={handlsetpassword} 
+    handelloginSubmit={handelloginSubmit}
+    />) 
   return(
     <>
     <Head><title>Cookie Stand Admin</title></Head>
-    <Header/><br/>
+    <Header handelSubmitlogout={handelSubmitlogout}/><br/>
     <Form 
     handelSubmit={handelSubmit}  
     Minimum={Minimum}
@@ -81,7 +128,7 @@ const [numberofitem,setNoOfItem]=useState(0)
     handlsetAverage={handlsetAverage}
     dataj={dataj}
     />
-    <TableReport hours={hours} dataj={dataj} total={total} totalsum={totalsum}/>
+    <TableReport hours={hours} dataj={dataj} total={total} totalsum={totalsum} handleDelete={handleDelete}/>
     <Footer numberofitem={numberofitem}/>
     </>
     
